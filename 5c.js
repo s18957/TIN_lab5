a534f9bdc49eafeacd7080aad7ede3fc0e097407
s18957 <61867603+s18957@users.noreply.github.com>
2020-11-20 16:30:05 +0100

    Update 5c.js

--- 5c.js
+++ 5c.js
@@ -1,20 +1,19 @@
-// let student = {
-//     courses: ["TIN", "BYT", "ZPR", "LEK"]
-// }
-// let names = ["Merit", "Gil", "Rica", "Woodie", "Bernadette"];
-//
-// function randomName() {
-//     return names[Math.random() * 5]
-// }
-//
-// let students = [];
-// function createNewStudents(limit) {
-//     for (let i = 1; i <= limit; i++) {
-//         let stud = Object.create(student);
-//         stud.fname = randomName();
-//         stud.lname = randomName();
-//         stud.id = i;
-//         students.push(stud);
-//     }
-// }
-// createNewStudents(10);
+ let student = {
+     courses: ["TIN", "BYT", "ZPR", "LEK"]
+ }
+ let names = ["Merit", "Gil", "Rica", "Woodie", "Bernadette"];
+
+ function randomName() {
+     return names[Math.random() * 5]
+ }
+
+ let students = [];
+ function createNewStudents(limit) {
+     for (let i = 1; i <= limit; i++) {/         let stud = Object.create(student);
+         stud.fname = randomName();
+         stud.lname = randomName();
+         stud.id = i;
+         students.push(stud);
+     }
+ }
+ createNewStudents(10);
